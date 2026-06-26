8c5c098e36d5b8557c65f12c6ceaa383d64930d4
Mervin Wang <wangbeeray@yahoo.com.sg>
2026-06-26 21:47:24 +0800

    [fix] adjust django setting of allowed hosts.

--- Orbital/Orbital/settings.py
+++ Orbital/Orbital/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-avvohf55!47g2ht0_-y*(+w%eukh@bf#024gyuo+r8xd-j7u!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backtesting-app-production.up.railway.app']
 
 
 # Application definition
