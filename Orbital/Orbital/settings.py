56af35b4a54c3a1fdc510e4516a53a148862278a
Mervin Wang <wangbeeray@yahoo.com.sg>
2026-06-26 21:50:47 +0800

    fixing local runserver

--- Orbital/Orbital/settings.py
+++ Orbital/Orbital/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-avvohf55!47g2ht0_-y*(+w%eukh@bf#024gyuo+r8xd-j7u!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['backtesting-app-production.up.railway.app']
+ALLOWED_HOSTS = ['backtesting-app-production.up.railway.app', '127.0.0.1']
 
 
 # Application definition
