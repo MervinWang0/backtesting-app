cd478bff2d7dfd29ddbc11c53c5f2517802f772d
Mervin Wang <wangbeeray@yahoo.com.sg>
2026-06-28 21:41:59 +0800

    Changed Whitenoise to not hash file path, suspected to cause js problems

--- Orbital/Orbital/settings.py
+++ Orbital/Orbital/settings.py
@@ -140,7 +140,7 @@ STORAGES = {
     # ...
     "staticfiles": {
         "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
-        "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+        "staticfiles": {"BACKEND": "whitenoise.storage.CompressedStaticFilesStorage"},
     },
 }
 
